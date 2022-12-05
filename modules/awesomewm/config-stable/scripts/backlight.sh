947b252a5dc62acccec0b22617cbed73bffb29b1
Albert Le Batteux <contact@albttx.tech>
2022-12-05 12:13:01 +0000

    chore: remove config stable

--- modules/awesomewm/config-stable/scripts/backlight.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-val=$(cat /sys/class/backlight/nv_backlight/brightness)
-if [ "$1" = "+" ] ; then
-  val=`expr $val + 5`
-else
-  val=`expr $val - 5`
-fi
-echo $val | sudo tee /sys/class/backlight/nv_backlight/brightness
