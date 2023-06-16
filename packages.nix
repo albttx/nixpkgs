6f71f0c5ed52c9e58a641a1235f28d46f0fba11c
Albert Le Batteux <contact@albttx.tech>
2023-06-16 10:24:29 +0100

    fix: install emacs with vterm

--- packages.nix
+++ packages.nix
@@ -1,8 +1,12 @@
 { config, pkgs, ... }:
 
 {
-  home.packages =  with pkgs; [
-    emacs
+  home.packages = with pkgs; [
+    # emacs
+    # 28.2 + native-comp
+    ((emacsPackagesFor emacsNativeComp).emacsWithPackages
+      (epkgs: [ epkgs.vterm ]))
+
     ripgrep
     tree
     jq
