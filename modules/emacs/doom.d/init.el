a9b8d1ca6b127e4f587e782e4a0fc07204d0eed5
albttx <contact@albttx.tech>
2023-10-31 15:06:55 +0000

    chore: enable module docker

--- modules/emacs/doom.d/init.el
+++ modules/emacs/doom.d/init.el
@@ -74,7 +74,7 @@
        ansible
        ;;(debugger +lsp)
        direnv
-       ;;docker
+       docker
        editorconfig      ; let someone else argue about tabs vs spaces
        ;;ein               ; tame Jupyter notebooks with emacs
        (eval +overlay)
