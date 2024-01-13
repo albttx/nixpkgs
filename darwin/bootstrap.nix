09ef8403e230deb2399861888a36ab177b6505d7
albttx <contact@albttx.tech>
2024-01-13 16:32:08 +0000

    chore: remove iohk cachix

--- darwin/bootstrap.nix
+++ darwin/bootstrap.nix
@@ -6,7 +6,6 @@
   nix.settings = {
     substituters = [
       "https://cache.nixos.org/"
-      "https://iohk.cachix.org"
       "https://hydra.iohk.io"
       "https://gfanton.cachix.org"
       "https://moul.cachix.org"
@@ -14,7 +13,6 @@
     ];
     trusted-public-keys = [
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-      "iohk.cachix.org-1:DpRUyj7h7V830dp/i6Nti+NEO2/nhblbov/8MW7Rqoo="
       "gfanton.cachix.org-1:i8zC+UjhhW5Wx2iRibhexJeBb1jOU/8oRFGG60IaAmI="
       "moul.cachix.org-1:jcmTECmIfe9zam+p4sP3RhEXmH7QTTChd9ax/vo1CYs="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
