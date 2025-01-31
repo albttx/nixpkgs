7e3bb801f06f22d6e1c36b375385092c8d240190
albttx <contact@albttx.tech>
2025-01-31 10:17:57 +0000

    chore: add tmtop

--- modules/dev/ops.nix
+++ modules/dev/ops.nix
@@ -18,6 +18,31 @@
     awscli2
     minio-client
     scaleway-cli
+
+    ## tmtop
+    (pkgs-master.buildGo122Module {
+      pname = "tmtop";
+      version = "v2.0.2";
+      src = pkgs.fetchFromGitHub {
+        owner = "QuokkaStake";
+        repo = "tmtop";
+        rev = "v2.0.2";
+        sha256 = "sha256-n/vn7zHB5p/GTalDM+H92pZevyGmUHGsO1b/aQnEb1E=";
+      };
+      subPackages = [ "cmd" ];
+      lsFlags = [ "-mod=mod" ];
+      vendorHash = "sha256-bXUg4mE5Cy0uKr095n2uvryDo3+LV/QP0XS/oiXe8+0=";
+
+      # Custom install phase to rename the binary
+      installPhase = ''
+        runHook preInstall
+        mkdir -p $out/bin
+        mv $GOPATH/bin/cmd $out/bin/tmtop
+        runHook postInstall
+      '';
+
+    })
+
   ];
 
 }
